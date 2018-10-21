547df59b9c1660981af61138602225447b24db17
MelnikVasya <melnik.vasya@gmail.com>
2018-10-21 22:59:53 +0300

    Suport Conjured items

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,49 +1,84 @@
 class GildedRose
+  STANDARD_QUALITY = 1
+  STANDARD_SELL_IN = 1
+  SKIP_DECREASE_SELL_IN = %w[Sulfuras].freeze
+
   def initialize(items)
     @items = items
   end
 
   def update_quality
     @items.each do |item|
-      if (item.name != 'Aged Brie') &&
-         (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              item.quality = item.quality + 1 if item.quality < 50
-            end
-            if item.sell_in < 6
-              item.quality = item.quality + 1 if item.quality < 50
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          item.quality = item.quality + 1 if item.quality < 50
-        end
-      end
+      update_item_quality(item)
+      decrease_sell_in(item)
     end
+  end
+
+  private
+
+  def update_item_quality(item)
+    case item.name
+    when /Aged Brie/
+      update_aged_brie_quality(item)
+    when /Backstage passes/
+      update_backstage_quality(item)
+    when /Sulfuras/
+      nil
+    when /Conjured/
+      update_conjured_quality(item)
+    else
+      decrease_quality(item)
+    end
+  end
+
+  def update_item_sell_in(item)
+    return if SKIP_DECREASE_SELL_IN.any? { |name| /#{name}/.match?(item.name) }
+
+    item.sell_in -= STANDARD_SELL_IN
+  end
+
+  def update_aged_brie_quality(item)
+    increase_quality(item)
+  end
+
+  def update_backstage_quality(item)
+    case item.sell_in
+    when (6..10)
+      increase_quality(item, 2)
+    when (1..5)
+      increase_quality(item, 3)
+    when 0
+      item.quality = 0
+    else
+      decrease_quality(item)
+    end
+  end
+
+  def update_conjured_quality(item)
+    amount = standard_quality(item) * 2
+    decrease_quality(item, amount)
+  end
+
+  def increase_quality(item, amount = nil)
+    amount ||= standard_quality(item)
+    increased_quality = item.quality + amount
+    return if increased_quality > 50
+
+    item.quality = increased_quality
+  end
+
+  def decrease_quality(item, amount = nil)
+    amount ||= standard_quality(item)
+    decreased_quality = item.quality - amount
+    return if decreased_quality.negative?
+
+    item.quality = decreased_quality
+  end
+
+  def standard_quality(item)
+    return STANDARD_QUALITY if item.sell_in > 0
+
+    STANDARD_QUALITY * 2
   end
 end
 
--- ruby/gilded_rose_tests.rb
+++ ruby/gilded_rose_tests.rb
@@ -7,15 +7,15 @@ class TestUntitled < Test::Unit::TestCase
     GildedRose.new(items).update_quality
     item = items.first
 
-    assert_equal(item.sell_in, 0)
-    assert_equal(item.quality, 0)
+    assert_equal(0, item.sell_in)
+    assert_equal(0, item.quality)
   end
 
   def test_if_sell_date_passed_quality_degrades_twice
     items = [Item.new('name', 0, 2)]
     GildedRose.new(items).update_quality
 
-    assert_equal(items.first.quality, 0)
+    assert_equal(0, items.first.quality)
   end
 
   def test_quality_cant_be_negative
@@ -29,21 +29,21 @@ class TestUntitled < Test::Unit::TestCase
     items = [Item.new('Aged Brie', 1, 0)]
     GildedRose.new(items).update_quality
 
-    assert_equal(items.first.quality, 1)
+    assert_equal(1, items.first.quality)
   end
 
   def test_aged_brie_quality_increases_by_2
     items = [Item.new('Aged Brie', 0, 0)]
     GildedRose.new(items).update_quality
 
-    assert_equal(items.first.quality, 2)
+    assert_equal(2, items.first.quality)
   end
 
   def test_quality_cannot_increase_more_than_50
     items = [Item.new('Aged Brie', 0, 50)]
     GildedRose.new(items).update_quality
 
-    assert_equal(items.first.quality, 50)
+    assert_equal(50, items.first.quality)
   end
 
   def test_sulfuras_dont_change_quality_and_sell_in
@@ -51,42 +51,42 @@ class TestUntitled < Test::Unit::TestCase
     GildedRose.new(items).update_quality
     item = items.first
 
-    assert_equal(item.sell_in, 1)
-    assert_equal(item.quality, 80)
+    assert_equal(1, item.sell_in)
+    assert_equal(80, item.quality)
   end
 
   def test_backstage_passes_quality_increases_by_2
     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 0)]
     GildedRose.new(items).update_quality
 
-    assert_equal(items.first.quality, 2)
+    assert_equal(2, items.first.quality)
   end
 
   def test_backstage_passes_quality_increases_by_3
     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 0)]
     GildedRose.new(items).update_quality
 
-    assert_equal(items.first.quality, 3)
+    assert_equal(3, items.first.quality)
   end
 
   def test_backstage_passes_quality_drops_to_0
     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 50)]
     GildedRose.new(items).update_quality
 
-    assert_equal(items.first.quality, 0)
+    assert_equal(0, items.first.quality)
   end
 
   def test_conjured_quality_degrades_by_2
     items = [Item.new('Conjured', 1, 2)]
     GildedRose.new(items).update_quality
 
-    assert_equal(items.first.quality, 0)
+    assert_equal(0, items.first.quality)
   end
 
   def test_conjured_quality_degrades_by_4
     items = [Item.new('Conjured', 0, 4)]
     GildedRose.new(items).update_quality
 
-    assert_equal(items.first.quality, 0)
+    assert_equal(0, items.first.quality)
   end
 end
